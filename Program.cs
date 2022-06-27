8d5004b7eff80078d839f2110409225ca740c7ec
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-06-27 14:25:42 +0200

    snack 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int num = 10;  
+int[] array = new int[num];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    sum += array[i];
+}
+
+Console.WriteLine(sum);
